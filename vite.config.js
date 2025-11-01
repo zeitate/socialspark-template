68d072b933b5f27427250099cfb9e78e0c6f92bb
zeitate <anonimnotoriu@protonmail.com>
2025-11-01 17:08:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-                            base: '/socialspark-template/', // ✅ Make sure this matches your repo name
+  base: '/socialspark-template/', // ✅ Make sure this matches your repo name
 })
