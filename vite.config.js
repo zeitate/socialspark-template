a72069888e0592df9bb9eaa9e97be2fe2bb2404f
zeitate <anonimnotoriu@protonmail.com>
2025-11-01 16:40:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GH_PAGES ? '/socialspark-react/' : '/',
+  base: '/socialspark-react/' : '/',
 })
